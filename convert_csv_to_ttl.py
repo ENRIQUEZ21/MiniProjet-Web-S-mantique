c8e47ef8bdff387d4f784b10d74767c08ff0cf3f
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-10-22 16:37:21 +0200

    comments

--- convert_csv_to_ttl.py
+++ convert_csv_to_ttl.py
@@ -14,15 +14,16 @@ outfile_ttl = open('exitTTLFile.ttl', 'a', encoding="utf8")
 outfile_ttl.write("@prefix d: <http://ex.org/data/> .\n")
 outfile_ttl.write("@prefix p: <http://ex.org/pred#> .\n\n")
 
+# Python will loop through each row in the csv file
 rownum = 0
 for row in reader:
     if (rownum == 0 or rownum == 1 or rownum == 2):
         pass
-    else:
+    else: # place the contents of the row into the 'c' variable, then create a 'd' that we will write in our TTL file
         c = row
         d = "d: p:est " + "d:"+c[0]  +", d:"+ c[1] +", d:"+  c[2] +", d:"+  c[3] +", d:"+  c[4]+".\n"
         outfile_ttl.write(d)
-    rownum+=1
+    rownum+=1 # add 1 to rownum to pass to following line
 
 outfile_ttl.close()
 tryFile.close()
